b0e7a590d04a0bb14c84ba1a72991d5601df10c4
squirlsgit <nich.t.j@gmail.com>
2017-07-31 21:05:37 -0700

    completes feature complete version of videolibrary. styling still needs to be implemented

--- app/javascript/packs/componentapps/dashboard.component.ts
+++ app/javascript/packs/componentapps/dashboard.component.ts
@@ -8,23 +8,30 @@ import { Response, Http, HttpModule, URLSearchParams, Headers, RequestOptions} f
 import 'rxjs/add/operator/map';
 import {Injectable} from "@angular/core";
 import { RouterModule, Routes, Router } from '@angular/router';
-
 @Component({
  selector: 'dashboard',
  template: `
 <template #library>
 
-<button (click)="toHome()"> back </button>
+<div class = "container">
+<div class = "row">
+<button (click)="toHome()" class = "btn-sm btn-success center-block">back</button></div>
 <div *ngIf = "reviewVideo" class = "jumbotron">
-<video  src = "activeVideo.url"></video>
-<button (click)="toggleselect(activeVideo)" class = "btn btn-primary btn-block"><td >Video: {{ video.url }}</td> <td> {{ video.path }} </td> <td> {{video.time}}</td></button>
+<div class = "row">
+<button (click)="toggledisplay(activeVideo[0])" class = "btn btn-danger btn-block">Video: {{ activeVideo[0].url }}, {{ activeVideo[0].path }}, {{activeVideo[0].time}} </button><div (click) = "toggleselectVideo(activeVideo[0])" class="checkbox">
+  <label><input type="checkbox" [checked] = "activeVideo[0].isSelect">Send Video</label>
+</div></div>
+<video  [src] = "activeVideo[0].url" class = "center-block" controls autoplay></video></div>
+<div *ngIf = "browse">
+  <div class = "row" *ngFor="let video of videos"  >
+    <button (click)="toggledisplay(video)" class = "btn btn-primary btn-block">Video: {{ video.url }}, {{ video.path }}, {{video.time}} </button><div (click) = "toggleselectVideo(video)" class="checkbox">
+      <label><input type="checkbox" [checked] = "video.isSelect">Send Video</label>
+    </div>
+  </div>
 </div>
-<table *ngIf = "browseLibrary">
-  <tr *ngFor="let video of videos"  >
-    <button (click)="toggleselect(video)" class = "btn btn-primary btn-block"><td >Video: {{ video.url }}</td> <td> {{ video.path }} </td> <td> {{video.time}}</td></button>
-  </tr>
-</table>
-<button (click)="sendVideos()">send</button>
+<div class = "row">
+<button (click)="sendVideos()" class = "btn-sm btn-success center-block">send</button></div>
+</div>
 </template>
 <!-- Home Screen Record Browse Admin -->
 <template #home>
@@ -58,21 +65,21 @@ import { RouterModule, Routes, Router } from '@angular/router';
    profilepic: string;
    playsrc: string;
    reviewVideo: boolean;
-   browseLibrary: boolean;
+   browse: boolean;
    activeVideo: any;
 
    constructor( public http: Http, private vcRef: ViewContainerRef, private router: Router) {
      this.refresh_switch = true;
      this.reviewVideo = false;
-     this.browseLibrary = true;
+     this.browse = true;
      this.videos = [];
      this.videostoSend = [];
+     this.activeVideo = [];
      this.profilepic = document.querySelector('view').getAttribute('profilepic'); //will not take information from the templated page unless forced.
    }
    ngOnInit(){
     this.vcRef.clear();
     this.vcRef.createEmbeddedView(this.displayHome);
-
    }
    startRecording(){
      this.router.navigate(['/record']);
@@ -84,34 +91,25 @@ import { RouterModule, Routes, Router } from '@angular/router';
      //httprequest with json, that sends the video_toSend array of videos to a dynamic page that can handle the information and
      alert(this.videostoSend[0].path);
    }
-    toggleselect(video: any){
+    toggledisplay(video: any){
      //console.log(video);
-     video.isSelect = !video.isSelect;
     //   console.log(video);
-
-    if(video.classList.contains('btn-primary')){
-      video.classList.remove('btn-primary');
-      video.classList.add('btn-danger');
-    }
-    else if(video.classList.contains('btn-danger')){
-      video.classList.remove('btn-danger');
-      video.classList.add('btn-primary');
-    }
-
-    if(video.isSelect) {
-      this.browseLibrary = false;
-       this.reviewVideo = true;
-       this.activeVideo = video;
-       video.isSelect = !video.isSelect;
-     } else {
-      this.reviewVideo = false;
-       this.browseLibrary = true;
-      }
-
+      this.browse? this.browse = false: this.browse = true
+      this.reviewVideo? this.reviewVideo = false: this.reviewVideo = true
+      this.reviewVideo? this.activeVideo.push(video): this.activeVideo = []
     //   console.log(video);
    }
+   toggleselectVideo(video: any){ console.log("integer1");
+     this.videos.forEach(shadow => {
+       console.log("integer2");
+       if(video.url === shadow.url ){
+         console.log("integer3");
+          shadow.isSelect = !shadow.isSelect;
+       }
+     });
+   }
 
-browseLibrary(){
+   browseLibrary(){
   this.vcRef.clear();
   this.vcRef.createEmbeddedView(this.displayLibrary);
   this.http.request('server/videolibrary.json').subscribe((res: Response) => {this.videos = res.json().videos});
@@ -138,6 +136,11 @@ display() {
      });
    }
   toHome(){
+    this.reviewVideo = false;
+    this.browse = true;
+    this.videos = [];
+    this.videostoSend = [];
+    this.activeVideo = [];
     this.status = "home";
     this.display();
     }
